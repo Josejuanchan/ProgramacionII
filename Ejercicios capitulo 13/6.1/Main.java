d54020599d733231afe2ab0277a0201d41596de2
Josejuanchan <148834418+Josejuanchan@users.noreply.github.com>
2023-11-05 13:47:35 -0600

    Update Main.java

--- Ejercicios capitulo 13/6.1/Main.java
+++ Ejercicios capitulo 13/6.1/Main.java
@@ -2,5 +2,11 @@ public class Main
 {
 	public static void main(String[] args) {
 	    
+	    Car car1 = new Car("Ford Mustang", 2019, "blue");
+	    Car car2 = new Car("Ford Mustang", 2019, "blue");
+	    Car car3 = new Car("Toyota Camry", 2018, "red");
+	    
+	    System.out.println(car1.equals(car2));
+	    System.out.println(car1.equals(car3));
 	}
 }
