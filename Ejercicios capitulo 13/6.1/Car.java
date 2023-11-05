b3751fda8a980ac8868aaa3295fad2d7785df8b7
Josejuanchan <148834418+Josejuanchan@users.noreply.github.com>
2023-11-05 13:42:31 -0600

    Add files via upload

---
+++ Ejercicios capitulo 13/6.1/Car.java
@@ -0,0 +1,20 @@
+public class Car{
+   
+    private String make; // modelo del automóvil
+    private int year; // año de lista del automóvil
+    private String color; // color del automóvi
+    
+    public Car(String make, int year, String color){
+        this.make = make;
+        this.year = year;
+        this.color = color;
+    }// end Car constructor
+    
+    public boolean equals(Car otherCar){
+        return otherCar != null &&
+        make.equals(otherCar.make) &&
+        year == otherCar.year &&
+        color.equals(otherCar.color);
+        
+    } // end equals
+} // end class Car
