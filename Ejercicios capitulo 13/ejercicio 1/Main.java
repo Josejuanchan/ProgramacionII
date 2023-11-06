6e1101e39b4f5e7d30a3b1d65cae86d8e2957f9b
Josejuanchan <148834418+Josejuanchan@users.noreply.github.com>
2023-11-05 18:23:06 -0600

    Update Main.java

--- Ejercicios capitulo 13/ejercicio 1/Main.java
+++ Ejercicios capitulo 13/ejercicio 1/Main.java
@@ -2,5 +2,15 @@ public class Main
 {
 	public static void main(String[] args) {
 	    
+	    Car car1 = new Car("Ford Mustang", 2019, "blue");
+	    Car car2 = new Car("Ford Mustang", 2019, "blue");
+	    Car car3 = new Car("Toyota Camry", 2018, "red");
+	    
+	    System.out.println(car1.equals(car2));
+	    System.out.println(car1.equals(car3));
+
+	    System.out.println("¿Es del mismo color los carros 1 y 2? " + car1.sameColorAs(car2));
+	    System.out.println("¿Es del mismo color los carros 1 y 3? " + car1.sameColorAs(car3));
+	    
 	}
 }
