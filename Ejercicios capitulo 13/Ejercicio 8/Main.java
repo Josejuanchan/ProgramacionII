ba254beab2d0943d3668f6801d6f0cea3ade057b
Josejuanchan <148834418+Josejuanchan@users.noreply.github.com>
2023-11-05 21:21:15 -0600

    Add files via upload

---
+++ Ejercicios capitulo 13/Ejercicio 8/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Animal[] animals = new Animal[20];
+        animals[0] = new Dog();
+        animals[1] = new Cat();	
+        Dog lassie = animals[0];
+        Cat fluffy = animals[1];
+
+	}
+}
