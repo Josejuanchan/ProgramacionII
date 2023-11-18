aba2a26c5ec231c5e1a90504dee29b6d5843822e
Josejuanchan <148834418+Josejuanchan@users.noreply.github.com>
2023-11-17 19:18:42 -0600

    Update Triangulo.java

--- Polimorfismo/graphics/Triangulo.java
+++ Polimorfismo/graphics/Triangulo.java
@@ -17,19 +17,21 @@ public class Triangulo extends FiguraGrafica {
 
     @Override
     public void dibujar(Graphics g) {
-        g.drawPolygon(x, y, 3 );
+        g.drawPolygon(x,y,3 );
     }
 
     @Override
     public void encojer(Graphics g) {
         x[1] +=5; y[1] -=5;
         x[2] -=5; y[2] -=5;
+        this.dibujar(g);
     }
 
     @Override
     public void agrandar(Graphics g) {
         x[1] -= 5; y[1] += 5;
-        x[2] += 5; y[2] += 5; 
+        x[2] += 5; y[2] += 5;
+        this.dibujar(g);
     }
 
     @Override
@@ -43,10 +45,10 @@ public class Triangulo extends FiguraGrafica {
         switch(direccion){
             case "arriba": y[0]-=5; y[1]-=5; y[2]-=5; break;
             case "abajo": y[0]+=5; y[1]+=5; y[2]+=5; break;
-            case "izquierda": x[0]-=5; x[1]-=5; x[2]-=5; break;
-            case "derecha":x[0]-=5; x[1]+=5; x[2]+=5;break;
+            case "izquierda" : x[0]-=5; x[1]-=5; x[2]-=5; break;
+            case "derecha" : x[0]-=5; x[1]+=5; x[2]+=5;break;
 	}
-	this.dibujar(g);  
+        this.dibujar(g);  
     }
     
 }
