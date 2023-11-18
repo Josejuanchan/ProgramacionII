4bf98e0a6bcbcf35f0e9fcef057ed7000593d76d
Josejuanchan <148834418+Josejuanchan@users.noreply.github.com>
2023-11-17 20:57:14 -0600

    Update Rectangulo.java

--- Polimorfismo/graphics/Rectangulo.java
+++ Polimorfismo/graphics/Rectangulo.java
@@ -1,5 +1,8 @@
 package graphics;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 /**
  * @author Héctor Quej Cosgaya
  * @author Jose Aguilar Canepa
@@ -7,5 +10,48 @@ package graphics;
  * Esta clase necesita de tu ayuda!
  */
 public class Rectangulo extends FiguraGrafica {
+
+    private int []x= {50,150,150,50};
+    private int []y= {50,50,100,100};
+    
+    @Override
+    public void dibujar(Graphics g) {
+        g.drawPolygon(x,y,4 );
+    }
+
+    @Override
+    public void encojer(Graphics g) {
+        x[0] +=5; y[1] -=5;
+        x[1] -=5; y[1] +=5;
+        x[2] -=5; y[2] -=5;
+        x[3] +=5; y[3] -=5;
+        this.dibujar(g);
+    }
+
+    @Override
+    public void agrandar(Graphics g) {
+        x[0] -=5; y[1] +=5;
+        x[1] += 5; y[1] -= 5;
+        x[2] += 5; y[2] += 5;
+        x[3] -= 5; y[3] += 5;
+        this.dibujar(g);
+    }
+
+    @Override
+    public void cambiarColor(Graphics g, Color c) {
+        g.setColor(c);
+        this.dibujar(g);
+    }
+
+    @Override
+    public void mover(Graphics g, String direccion) {
+        switch(direccion){
+            case "arriba": y[0]-=5; y[1]-=5; y[2]-=5; y[3] -=5; break;
+            case "abajo": y[0]+=5; y[1]+=5; y[2]+=5; y[3] +=5; break;
+            case "izquierda" : x[0]-=5; x[1]-=5; x[2]-=5; x[3]-=5; break;
+            case "derecha" : x[0]+=5; x[1]+=5; x[2]+=5; x[3]+=5;break;
+	}
+        this.dibujar(g);  
+    }
     
 }
