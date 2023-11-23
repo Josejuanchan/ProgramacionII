0e394421e11d0e9f558bfe3bef4734cce738d7ea
Josejuanchan <148834418+Josejuanchan@users.noreply.github.com>
2023-11-22 22:44:08 -0600

    Update Controladora.java

--- Archivos/Controladora.java
+++ Archivos/Controladora.java
@@ -4,12 +4,12 @@ import java.io.*;
 public class Controladora{
     
 		public static void main(String[] args) {
-            WebMaker generador = new WebMaker();
+            /* WebMaker generador = new WebMaker();
 	   
             try{
             
             PrintWriter escritor = new PrintWriter("index.html");
-	    Scanner lector = new Scanner(new FileReader("form.txt"));
+			Scanner lector = new Scanner(new FileReader("form.txt"));
 		
             escritor.println(generador.imprimirEncabezado());
 		
@@ -20,9 +20,26 @@ public class Controladora{
             }
                 escritor.println(generador.imprimirCierre());
                 lector.close();
-                escritor.close();
-		
-	} catch (FileNotFoundException e){
-        }
-    }
-}
+                escritor.close(); 
+				catch (FileNotFoundException e){
+			}*/
+				
+				
+			try{
+			DataOutputStream escritor = new DataOutputStream (new FileOutputStream("hola.bin"));
+			escritor.writeInt(33465);
+			escritor.writeChar('a');
+			escritor.writeChar('z');
+			escritor.writeShort(10);
+			escritor.close();
+			DataInputStream	lector = new DataInputStream (new FileInputStream("hola.bin"));
+			System.out.println(lector.readInt());
+			System.out.println(lector.readChar());
+			System.out.println(lector.readChar());
+			System.out.println(lector.readShort());
+			escritor.close();
+			}catch (IOException e) {
+			
+			}
+		} 	
+	}
